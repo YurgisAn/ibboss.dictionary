0bdb5c08c26b44a3b27109a557c091359de2ca57
Василий Воронков (4084069) <vvoronkov@vtb.ru>
2022-12-23 13:12:45 +0300

    -

---
+++ Ogress.Sourcebook/Ogress.Sourcebook.Api/ExceptionMiddleware.cs
@@ -0,0 +1,27 @@
+﻿using System.Net;
+
+namespace Ogress.Sourcebook.Api;
+
+public sealed class ExceptionMiddleware
+{
+    private readonly RequestDelegate next;
+
+    public ExceptionMiddleware(RequestDelegate next)
+    {
+        this.next = next;
+    }
+
+    public async Task Invoke(HttpContext context)
+    {
+        try
+        {
+            await next(context);
+        }
+        catch (Exception ex)
+        {
+            context.Response.ContentType = "text/plain";
+            context.Response.StatusCode = (int)HttpStatusCode.BadRequest;
+            await context.Response.WriteAsync(ex.ToString());
+        }
+    }
+}
--- Ogress.Sourcebook/Ogress.Sourcebook.Api/Program.cs
+++ Ogress.Sourcebook/Ogress.Sourcebook.Api/Program.cs
@@ -12,7 +12,6 @@ namespace Ogress.Sourcebook.Api
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
-
             var app = builder.Build();
 
             // Configure the HTTP request pipeline.
@@ -22,13 +21,10 @@ namespace Ogress.Sourcebook.Api
                 app.UseSwaggerUI();
             }
 
+            app.UseMiddleware<ExceptionMiddleware>();
             app.UseHttpsRedirection();
-
             app.UseAuthorization();
-
-
             app.MapControllers();
-
             app.Run();
         }
     }
